f085238548969648405fddbd4685b04b4b30b2ba
vasyl.pashko <vasyl.pashko@gmail.com>
2023-05-31 07:54:00 +0300

    first steps on creating form

---
+++ src/app/card-form/card-form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-card-form',
+  templateUrl: './card-form.component.html',
+  styleUrls: ['./card-form.component.css'],
+})
+export class CardFormComponent {
+  cardForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    cardNumber: new FormControl(''),
+    expiration: new FormControl(''),
+    securityCode: new FormControl(''),
+  });
+}
